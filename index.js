a7778bb1b3d468b5a772a6739c26e89c5a848bdb
ParasPandey019 <paraspandey019@gmail.com>
2023-09-08 14:04:14 +0530

    added ejs layouts and partials

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+app.use(express.static('./assets'));
 
 // using express router
 app.use('/',require("./routes/index"));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if(err){
@@ -13,4 +24,6 @@ app.listen(port, function(err){
 
     console.log(`Server is up and running on port ${port}`);
 })
+
+
 
