bf4c04b8e8468cdf1e1a6d5e538ae731ecbca5aa
ParasPandey019 <paraspandey019@gmail.com>
2023-09-22 14:29:07 +0530

    comments controller and comments feature added

---
+++ controllers/comments_controller.js
@@ -0,0 +1,18 @@
+const Comment = require("../models/comments");
+const Post = require("../models/post");
+
+module.exports.create = async function(req,res){
+    const currPost = await Post.findById(req.body.post);
+    if(currPost){
+        const newComment = await Comment.create({
+            content: req.body.content,
+            post: req.body.post,
+            user: req.user._id
+        });
+        currPost.comments.push(newComment);
+        await currPost.save();
+        await newComment.save();
+        return res.redirect('/');
+    }
+    
+}
