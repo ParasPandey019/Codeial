160f7a150866fe9f312d41385f062598469082d8
ParasPandey019 <paraspandey019@gmail.com>
2023-09-23 13:09:18 +0530

    populating comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -16,3 +16,4 @@ module.exports.create = async function(req,res){
     }
     
 }
+
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,14 @@ const Post = require('../models/post')
 module.exports.home = async function(req,res){
     // const posts = await Post.find({});
 
-    const posts = await Post.find({}).populate('user').exec()
+    const posts = await Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    }).exec()
 
     return res.render('home',{
         title: "Codeial | Home",
